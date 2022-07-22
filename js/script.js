09930f105a34d416ff5a0f0b3ca5a9f8b8a0fe79
Brayce <braycefepa@gmail.com>
2022-07-22 07:51:43 +0100

    ajout des pages login, register et repas

---
+++ js/script.js
@@ -0,0 +1,62 @@
+fetch('./php/repas.php', {
+    method: 'GET',
+})
+.then(response => response.json())
+.then(responseObject =>{
+    console.log(responseObject);
+
+    responseObject.forEach(obj => {
+
+        let img = document.createElement('img');
+
+        let foodCard = document.createElement('div');
+        foodCard.setAttribute('class', 'food-card');
+
+        let h4 = document.createElement('h4');
+
+        let cardFoot = document.createElement('div');
+        cardFoot.setAttribute('class', 'card-foot');
+
+        let h3 = document.createElement('h3');
+
+        let a = document.createElement('a');
+        a.setAttribute('href', '');
+        a.setAttribute('class', 'btn f-btn');
+
+
+
+
+        img.setAttribute('src', `${obj.image}`);
+
+        h4.textContent = `${obj.name}`;
+
+        h3.textContent = `${obj.price}`;
+
+        a.textContent = `${obj.add}`;
+
+        cardFoot.appendChild(h3);
+        cardFoot.appendChild(a);
+
+
+        foodCard.appendChild(img);
+        foodCard.appendChild(h4);
+        foodCard.appendChild(cardFoot);
+
+
+        let foodRow = document.getElementById('food-row');
+
+        foodRow.appendChild(foodCard);
+
+        
+
+
+
+
+
+
+    })
+
+
+}).catch(error => {
+    console.error(error);
+});
---
+++ php/repas.php
@@ -0,0 +1,97 @@
+<?php
+
+$data = array(
+
+    "0" => array(
+        "image" => "images/product_2.3.jpg",
+        "name" => "chicken burger",
+        "price" => "3000 fcfa",
+        "add" => "Add to cart",
+    ),
+
+    "1" => array(
+        "image" => "images/product_01.1.jpg",
+        "name" => "royal cheese burger",
+        "price" => "1500 fcfa",
+        "add" => "Add to cart",
+    ),
+
+    "2" => array(
+        "image" => "images/product_01.3.jpg",
+        "name" => "classic hamburger",
+        "price" => "1500 fcfa",
+        "add" => "Add to cart",
+    ),
+
+    "3" => array(
+        "image" => "images/product_01.jpg",
+        "name" => "cheese burger",
+        "price" => "1500 fcfa",
+        "add" => "Add to cart",
+    ),
+
+
+    "4" => array(
+        "image" => "images/product_2.1.jpg",
+        "name" => "vegetarian pizza",
+        "price" => "4500 fcfa",
+        "add" => "Add to cart",
+    ),
+
+
+    "5" => array(
+        "image" => "images/product_2.2.jpg",
+        "name" => "chicken burger",
+        "price" => "3000 fcfa",
+        "add" => "Add to cart",
+    ),
+
+
+    "6" => array(
+        "image" => "images/product_4.3.png",
+        "name" => "maxican green wave",
+        "price" => "3000 fcfa",
+        "add" => "Add to cart",
+    ),
+
+
+    "7" => array(
+        "image" => "images/product_3.1.jpg",
+        "name" => "double cheese margherita",
+        "price" => "4500 fcfa",
+        "add" => "Add to cart",
+    ),
+
+
+    "8" => array(
+        "image" => "images/product_4.2.jpg",
+        "name" => "pizza with mushroom",
+        "price" => "4500 fcfa",
+        "add" => "Add to cart",
+    ),
+
+
+    "9" => array(
+        "image" => "images/product_2.3.jpg",
+        "name" => "chicken burger",
+        "price" => "3000 fcfa",
+        "add" => "Add to cart",
+    ),
+
+    "10" => array(
+        "image" => "images/product_01.1.jpg",
+        "name" => "royal cheese burger",
+        "price" => "1500 fcfa",
+        "add" => "Add to cart",
+    ),
+
+    "11" => array(
+        "image" => "images/product_01.3.jpg",
+        "name" => "classic hamburger",
+        "price" => "1500 fcfa",
+        "add" => "Add to cart",
+    ),
+
+);
+
+echo json_encode($data);
